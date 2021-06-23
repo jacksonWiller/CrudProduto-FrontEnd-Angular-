53675b56af60d06a38a00eaa89410af5c6a6d6d3
Jackson Duarte <jacksonwillerduarte@gmail.com>
2021-06-23 19:40:45 -0300

    reactive form funcionando

--- src/app/components/produtos/produto-add/produto-add.component.ts
+++ src/app/components/produtos/produto-add/produto-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-produto-add',
@@ -6,11 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./produto-add.component.scss']
 })
 export class ProdutoAddComponent implements OnInit {
+  form!: FormGroup;
+  get name() { return this.profileForm.get('firstName')};
+  
+  profileForm = new FormGroup({
+    firstName: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+    lastName: new FormControl(''),
+  });
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.validation();
   }
+
+  public validation(): void {
+    this.form = new FormGroup({
+      nome: new FormControl(
+      [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+      quantidade: new FormControl('')
+    })
+  }
+
+  get nome() { return this.form.get('nome') };
 
   salvarAlteracao(){
 
